6f9c11336edf4ea51c901cf64971bbedeb5d1a2a
Alex Pham <alexanderpham023@gmail.com>
2024-11-08 12:00:49 -0800

    saved version for stagger animation triggers

--- frontend/src/app/Pages/landing/landing.component.ts
+++ frontend/src/app/Pages/landing/landing.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AboutCardsComponent } from '../../components/about-cards/about-cards.component';
 import {
   trigger,
   state,
   style,
   animate,
-  transition,
+  transition, query, stagger
 } from '@angular/animations';
 
 @Component({
@@ -20,15 +20,56 @@ import {
       state('open', style({ transform: 'translateX(0)' })),  // Same here
       transition('close => open', [animate('1s ease-in')]),
     ]),
+
+    trigger('loadTrigger', [
+      state('hidden', style({
+        transform: 'translateX(100%)', opacity: 0})
+      ), state('shown', style({
+        transform: 'translateX(0%)', opacity:100})
+      ), transition('hidden => shown', [
+        animate('1s ease-in')
+      ]),]),
+
+    trigger("staggerTrigger", [
+      transition( '* <=> *', [
+        query(':enter', [
+          style({opacity: 1, transform: 'scale(0.7)'}),
+          stagger(100, [
+            animate('500ms ease-in', style({opacity:1, transform:'scale(1)'}))
+          ],), 
+        ], {optional:true}),
+
+        query(':leave', [
+          style({opacity: 1, transform: 'scale(1)'}),
+          stagger(-100, [
+            animate('500ms ease-in', style({opacity:0, transform:'scale(0.7)'}))
+          ],), 
+        ], {optional:true})
+      ])
+    ]),
+
   ],
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit{
   test: string = 'test';
   test2: string = 'testing';
   protected cardState: 'open' | 'close' = 'close';
+  protected state: 'shown' | 'hidden' = 'hidden';
 
+  nums = [0]
   clicked()
   {
     this.cardState='open';
+  }
+
+  add()
+  {
+    this.nums = [...this.nums, this.nums.length + 1]
+  }
+
+  ngAfterViewInit() {
+    setTimeout( () => {
+    this.state = 'shown';
+    }, 1000);
   }
 }
